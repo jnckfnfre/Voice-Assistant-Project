5612c4a56f3cdf6df94f4b3287b1d95e4c97c55f
nahyanmunawar <urface2838@gmail.com>
2023-12-25 15:05:28 -0600

    gave up on pattern2 approach. reminders must be in "remind me to --> task --> time" or "set a reminder to --> task --> time." every variation of time works, "tomorrow, thursday, tomorrow at 3:00 pm, at 3:00 pm tomorrow, etc."

--- ExtractingTaskAndDate.py
+++ ExtractingTaskAndDate.py
@@ -17,46 +17,47 @@ pattern1 = [
     {"IS_ALPHA": True, "OP": "*"},
 ]
 
-# Updated Pattern for sentences starting with a time or date
-pattern2 = [
-    {"POS": {"IN": ["ADP", "NOUN", "NUM", "DET"]}, "OP": "*"},
-    {"TEXT": {"IN": ["remind", "set"]}, "OP": "?"},
-    {"TEXT": "me", "OP": "?"},
-    {"TEXT": "to"},
-    {"IS_ALPHA": True, "OP": "+"},
-    {"TEXT": "'s", "OP": "?"},
-    {"IS_ALPHA": True, "OP": "*"},
-]
-
-# Add patterns to the matcher
-matcher.add("REMINDER_TASK", [pattern1, pattern2])
+# Add pattern1 to the matcher
+matcher.add("REMINDER_TASK", [pattern1])
 
 # Test sentences
 texts = [
     "Remind me to go running at 5:00 a.m. tomorrow.",
     "Set a reminder to throw the trash at 8:00 p.m. today.",
-    "On Thursday at 10:00 a.m., remind me to go to school.",
-    "At 10:00 a.m. on Thursday, remind me to pick up my sister.",
     "Remind me to get everyone's number at 8:00 p.m. on Tuesday.",
-    "Set a reminder to wash my dad's car at 10:00 tomorrow.",
+    "Set a reminder to wash my dad's car at 10:00 a.m. tomorrow.",
     "Remind me to not fall asleep in class at 1:15 p.m. today.",
-    "Remind me to take a nap before class at 2:25 p.m tomorrow."
+    "Remind me to take a nap before class on Thursday at 2:00 p.m.",
+    "Remind me to work out tomorrow at 10:00 a.m.",
+    "Remind me to get two eggs tomorrow at 8:00 a.m.",
+    "Set a reminder to go to sleep at 10:00 p.m.",
+    "Remind me to go home after class at 3:00 p.m. tomorrow"
 ]
+
 # Time-related words for filtering tasks
-time_words = ["at", "on", "in", "by", "before", "after"]
+time_words = ["at", "on"]
 
-# Apply the matcher to each text and process tasks
-for text in texts:
-    doc = nlp(text)
-    matches = matcher(doc)
+# Phrases to stop including in the task
+stop_phrases = ["tomorrow at", "on Monday at", "on Tuesday at", "on Wednesday at", "on Thursday at", "on Friday at", "on Saturday at", "on Sunday at"]
 
+# Settings for dateparser
+dateparser_settings = {
+    "PREFER_DATES_FROM": "future"
+}
+
+def pattern1_approach(doc, matches):
     tasks = []
     time_strings = []
-
     for match_id, start, end in matches:
-        # Extracting the task
+        # Existing logic for extracting the task
         task_start = [tok.i for tok in doc[start:end] if tok.text.lower() == "to"][0] + 1
-        task = doc[task_start:end].text
+        task_end = start
+        for i in range(start, end):
+            if doc[i].lower_ in time_words or (doc[i].ent_type_ in ["TIME", "DATE"]):
+                task_end = i
+                break
+
+        task = doc[task_start:task_end].text
         tasks.append(task)
 
         # Extracting the time part
@@ -64,13 +65,13 @@ for text in texts:
         time_string = doc[time_start:].text
         time_strings.append(time_string)
 
-    # Filter for the longest unique task and remove trailing time word if present
+    # Filter for the longest unique task
     if tasks:
         longest_task = max(tasks, key=len)
-        task_words = longest_task.split()
-        if task_words[-1].lower() in time_words:
-            longest_task = " ".join(task_words[:-1])
-
-        # Parsing the time string
-        reminder_time = dateparser.parse(time_strings[tasks.index(longest_task)])
+        reminder_time = dateparser.parse(time_strings[tasks.index(longest_task)], settings=dateparser_settings)
         print(f"Reminder Task: {longest_task}, Reminder Time: {reminder_time}")
+
+for text in texts:
+    doc = nlp(text)
+    matches = matcher(doc)
+    pattern1_approach(doc, matches)
